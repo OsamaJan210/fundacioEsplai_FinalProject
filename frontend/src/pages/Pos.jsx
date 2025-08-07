8845a2395f845058e5baa82a382213152a68c909
aitorbares018 <aitorbares018@gmail.com>
2025-08-07 14:05:04 +0200

    const api

--- frontend/src/pages/Pos.jsx
+++ frontend/src/pages/Pos.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Pos.css";
 
-const API_URL = "http://192.168.0.236:8081";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function Pos() {
   const navigate = useNavigate();
